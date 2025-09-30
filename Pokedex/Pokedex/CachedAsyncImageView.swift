87f29203ed307083428a5420264f59c4ea6ef86b
asaini987 <sainiraaditya@gmail.com>
2025-09-29 22:21:39 -0400

    implemented image caching with URLCache

---
+++ Pokedex/Pokedex/CachedAsyncImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CachedAsyncImageView.swift
+//  Pokedex
+//
+//  Created by Aaditya Saini on 9/29/25.
+//
+
+import SwiftUI
+
+struct CachedAsyncImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CachedAsyncImageView()
+}
